5deccfe5b281bdcd4086e107f334217e6a4d7e75
jennn <kirk.jenn@gmail.com>
2015-05-10 11:45:24 -0700

    Added checks on sample.id and snp.id

---
+++ R/check.snp.R
@@ -0,0 +1,16 @@
+# check.vector function
+
+check.snp <- function(user.snp, data.snp)
+{
+  if (length(user.snp) > length(data.snp)){
+    stop("More SNP IDs given than are in the genotype data set.")
+  }else if (length(user.snp) <= 0){
+    stop("Snp.id vector specified has length of 0.")
+  }else if (any(!(user.snp %in% data.snp)))
+  {
+    stop("Snp.id vector specified has SNP IDs not in the data set.")
+  }else{
+    return(TRUE)
+  }
+  
+}
