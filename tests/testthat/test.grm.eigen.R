196b69a64cf77d88c41bea89f39555eff68173eb
jennn <kirk.jenn@gmail.com>
2015-05-10 11:46:35 -0700

    Added separate code to generate test cases for GRMs

--- tests/testthat/test.grm.eigen.R
+++ tests/testthat/test.grm.eigen.R
@@ -1,57 +1,91 @@
 # Test for grm.eigen
 
 # Exmple 1
-test <- t(matrix(c(0,0,1, 0,1,0, 1,1,1), ncol = 3, byrow = TRUE))
-# create a test gds file
-snpgdsCreateGeno("test.gds", genmat = test,
-                 sample.id = 1:3, snp.id = 1:3,
-                 snp.chromosome = rep(1,3),
-                 snp.position = 1:3,
-                 snp.allele = rep(0, 3) )
-test.file <- snpgdsOpen("test.gds")
+# read in the solutions
+snprelate.res <- readRDS("snprelate-example-1.txt")
+my.res <- readRDS("eigen-example-1.txt")
 
+# open the test file
+test.file <- snpgdsOpen("ex1.gds")
 samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
 snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
 
-# SNPRelate reports a GRM that is multiplied by (n-1)/trace(GRM)
-my.eigen <- grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
-          remove.monosnp = FALSE, maf = NaN, missing.rate = NaN)
-scale <- (ncol(my.eigen)-1)/matrix.trace(my.eigen)
+test_that("grm.eigen returns the appropriate GRM matrix and that it is equivalent to SNPRelate",
+{
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                             remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = FALSE), 
+                   my.res)
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = FALSE), 
+               snprelate.res)
+})
+snpgdsClose(test.file)
 
-snprelate.res <- snpgdsPCA(test.file, genmat.only = TRUE)
+
+
+# Exmple 1 Transposed
+# read in the solutions
+snprelate.res <- readRDS("snprelate-example-1t.txt")
+my.res <- readRDS("eigen-example-1t.txt")
+
+# open the test file
+test.file <- snpgdsOpen("ex1t.gds")
+samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
+snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
+
+test_that("grm.eigen returns the appropriate GRM matrix and that it is equivalent to SNPRelate",
+{
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = TRUE), 
+               my.res)
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = TRUE), 
+               snprelate.res)
+})
+snpgdsClose(test.file)
+
+
+
+# Example 2
+# read in the solutions
+snprelate.res <- readRDS("snprelate-example-2.txt")
+my.res <- readRDS("eigen-example-2.txt")
+
+# open the test file
+test.file <- snpgdsOpen("ex2.gds")
+samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
+snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
 
 test_that("grm.eigen returns the appropriate GRM matrix",
 {
   expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
-                             remove.monosnp = FALSE, maf = NaN, missing.rate = NaN), 
-                   snprelate.res$genmat/scale)
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = FALSE), 
+               my.res)
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = FALSE), 
+               snprelate.res)
 })
-unlink("test.gds")
+snpgdsClose(test.file)
 
 
-# Example 2
-data(hapmap_geno)
-snpgdsCreateGeno("test2.gds", genmat = hapmap_geno$genotype[1:5, 1:3],
-                 sample.id = hapmap_geno$sample.id[1:3], snp.id = hapmap_geno$snp.id[1:5],
-                 snp.chromosome = hapmap_geno$snp.chromosome[1:5],
-                 snp.position = hapmap_geno$snp.position[1:5],
-                 snp.allele = hapmap_geno$snp.allele[1:5])
-test2.file <- snpgdsOpen("test2.gds")
 
-samp.id <- read.gdsn(index.gdsn(test2.file, "sample.id"))
-snp.id <- read.gdsn(index.gdsn(test2.file, "snp.id"))
+# Example 2 Transposed
+# read in the solutions
+snprelate.res <- readRDS("snprelate-example-2t.txt")
+my.res <- readRDS("eigen-example-2t.txt")
 
-# SNPRelate reports a GRM that is multiplied by (n-1)/trace(GRM)
-my.res <- grm.eigen(test2.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
-                    remove.monosnp = FALSE, maf = NaN, missing.rate = NaN) 
-scale <- (ncol(my.res)-1)/matrix.trace(my.res)
-
-snprelate.res <- snpgdsPCA(test2.file, genmat.only = TRUE)
+# open the test file
+test.file <- snpgdsOpen("ex2t.gds")
+samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
+snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
 
 test_that("grm.eigen returns the appropriate GRM matrix",
 {
-  expect_equal(grm.eigen(test2.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
-                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN), 
-               snprelate.res$genmat/scale)
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = TRUE), 
+               my.res)
+  expect_equal(grm.eigen(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = TRUE), 
+               snprelate.res)
 })
-unlink("test2.gds")
+snpgdsClose(test.file)
--- tests/testthat/test.grm.pcaseq.R
+++ tests/testthat/test.grm.pcaseq.R
@@ -1,54 +1,72 @@
 # Test for grm.eigen
 
-# Example 1
-test <- t(matrix(c(0,0,1, 0,1,0, 1,1,1), ncol = 3, byrow = TRUE))
+# Exmple 1
+# read in the solutions
+my.res <- readRDS("pcaseq-example-1.txt")
 
-# create a test gds file
-snpgdsCreateGeno("test.gds", genmat = test,
-                 sample.id = 1:3, snp.id = 1:3,
-                 snp.chromosome = rep(1,3),
-                 snp.position = 1:3,
-                 snp.allele = rep(0, 3) )
-test.file <- snpgdsOpen("test.gds")
-
+# open the test file
+test.file <- snpgdsOpen("ex1.gds")
 samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
 snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
-
-# read in the answer I calculated by hand--note that we use scan and
-# not read.table because read.table creates a matrix with dimnames
-# which causes the check to fail
-answer <- matrix(scan("pcaseq-example-1.txt"), ncol = 3, byrow = TRUE)
 
 test_that("grm.pcaseq returns the appropriate GRM matrix",
 {
   expect_equal(grm.pcaseq(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
-                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN), 
-              answer)
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = FALSE), 
+              my.res)
 })
-unlink("test.gds")
+snpgdsClose(test.file)
+
+
+
+# Example 1 Transposed
+my.res <- readRDS("pcaseq-example-1t.txt")
+
+# open the test file
+test.file <- snpgdsOpen("ex1t.gds")
+samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
+snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
+
+test_that("grm.pcaseq returns the appropriate GRM matrix",
+{
+  expect_equal(grm.pcaseq(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                          remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = TRUE), 
+               my.res)
+})
+snpgdsClose(test.file)
+
 
 
 # Example 2
-data(hapmap_geno)
-snpgdsCreateGeno("test2.gds", genmat = hapmap_geno$genotype[1:5, 1:3],
-                 sample.id = hapmap_geno$sample.id[1:3], snp.id = hapmap_geno$snp.id[1:5],
-                 snp.chromosome = hapmap_geno$snp.chromosome[1:5],
-                 snp.position = hapmap_geno$snp.position[1:5],
-                 snp.allele = hapmap_geno$snp.allele[1:5])
-test2.file <- snpgdsOpen("test2.gds")
+# read in the solutions
+my.res <- readRDS("pcaseq-example-2.txt")
 
-samp.id <- read.gdsn(index.gdsn(test2.file, "sample.id"))
-snp.id <- read.gdsn(index.gdsn(test2.file, "snp.id"))
-
-
-# read in the answer I calculated by hand--note that we use scan and
-# not read.table because read.table creates a matrix with dimnames
-# which causes the check to fail
-answer <- matrix(scan("pcaseq-example-2.txt"), ncol = 3, byrow = TRUE)
+# open the test file
+test.file <- snpgdsOpen("ex2.gds")
+samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
+snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
 
 test_that("grm.eigen returns the appropriate GRM matrix",
 {
-  expect_equal(grm.pcaseq(test2.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
-                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN), 
-               answer)
+  expect_equal(grm.pcaseq(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                         remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = FALSE), 
+               my.res)
 })
+snpgdsClose(test.file)
+
+
+# Example 2 Transposed
+my.res <- readRDS("pcaseq-example-2t.txt")
+
+# open the test file
+test.file <- snpgdsOpen("ex2t.gds")
+samp.id <- read.gdsn(index.gdsn(test.file, "sample.id"))
+snp.id <- read.gdsn(index.gdsn(test.file, "snp.id"))
+
+test_that("grm.pcaseq returns the appropriate GRM matrix",
+{
+  expect_equal(grm.pcaseq(test.file, sample.id = samp.id, snp.id = snp.id, autosome.only = FALSE, 
+                          remove.monosnp = FALSE, maf = NaN, missing.rate = NaN, transpose = TRUE), 
+               my.res)
+})
+snpgdsClose(test.file)
