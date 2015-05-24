fa4056cebefcfa52636964e3eed336b3e06b1d39
jennn <kirk.jenn@gmail.com>
2015-05-23 21:29:01 -0700

    Revised eigen count to allow a short cut for all eigenvectors
    Zero now indicates return all eigenvectors

--- tests/testthat/test.checkEcnt.R
+++ tests/testthat/test.checkEcnt.R
@@ -4,13 +4,13 @@ test_that("checkEcnt passes when the input is a proper missingness rate",
 {
   expect_true(checkEcnt(1))
   expect_true(checkEcnt(57))
+  expect_true(checkEcnt(0))
 })
 
 test_that("checkEcnt fails when the input is not correct", 
 {
   expect_error(checkEcnt("cake"))
   expect_error(checkEcnt(1:3))
-  expect_error(checkEcnt(0))
   expect_error(checkEcnt(-5))
   expect_error(checkEcnt(NA))
   expect_error(checkEcnt(NULL))
