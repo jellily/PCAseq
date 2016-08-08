cf934d80a038de5c23597afe03d391e39f40013b
jellily <jlkirk@uw.edu>
2016-08-08 14:43:16 -0700

    Fix the seqGRM function so that it actually outputs the grm.

--- R/seqGRM.R
+++ R/seqGRM.R
@@ -15,14 +15,19 @@ seqGRM <- function(gdsobj, weights, sample.id = NULL, snp.id = NULL,
 
 
   # Find the GRM
-  grm <- runGRM(gdsobj, weights, sample.id, snp.id, autosome.only, 
+  grmRes <- runGRM(gdsobj, weights, sample.id, snp.id, autosome.only, 
                 remove.monosnp, maf, missing.rate)
 
+  grm <- grmRes[[1]]
+  sampleID <- grmRes[[2]]
+  snpID <- grmRes[[3]]
+    
+    
   # check if the GRM only has one entry
   if (dim(grm)[1] == 1 | dim(grm)[2] == 1 | class(grm) != "matrix") {
     warning("GRM has only one entry.")
   }
-
+  
   # Return the appropriate object
   # eigen.cnt and need.genmat
   seqPCAClass(grm, weights, maf, sample.id, snp.id, eigenRes = NULL,
